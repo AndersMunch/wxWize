659ed2b5aea6d5cfa616b2392904d628ac45401f
Anders Munch <anders@jmunch.dk>
2021-03-15 22:39:28 +0100

    Version 1.2.0

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ with open('../README.rst','rb') as f:
 
 setup(
     name='wxWize',
-    version='1.1.1',
+    version='1.2.0',
     description='wxPython object builder',
     long_description=long_description,
     url='https://github.com/AndersMunch/wxWize',
